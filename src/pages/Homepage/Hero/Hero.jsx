56c9bdde2908727cac0106b8030e370b54699412
Mayor-Isaac <isaacogunyileka@gmail.com>
2025-08-01 19:49:23 +0100

    fix: Hero section

--- src/pages/Homepage/Hero/Hero.jsx
+++ src/pages/Homepage/Hero/Hero.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import SubHero1 from "./SubHero1";
 import SubHero2 from "./SubHero2";
 
 const Hero = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  // Array of slides
+  const slides = [
+    {
+      id: 0,
+      component: <SubHero1 />
+    },
+    {
+      id: 1,
+      component: <SubHero2 />
+    }
+  ]
+  useEffect(() => {
+    
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+    }, 4000); // Change every 2s
+    return () => clearInterval(interval);
+  }, [slides.length]);
   return (
-    <div className="flex w-[200vw] h-screen overflow-hidden font-poppins">
-      <SubHero1 />
-      <SubHero2 />
+    <div className="flex w-screen h-screen overflow-hidden font-poppins relative">
+      {slides.map((slide) => (
+        <div
+          key={slide.id}
+          className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${
+            currentSlide === slide.id ? "opacity-100 z-20" : "opacity-0 z-10"
+          }`}
+        >
+          {slide.component}
+        </div>
+      ))}
     </div>
   );
 };
